b9b316ebe6bf2733aeb4ec2fe0b337a5539a96f8
Johan Thorell <jo1han@hotmail.com>
2013-09-09 14:36:22 +0200

    xcode project and backend-folder

---
+++ SchemeApp/SchemeApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SchemeApp
+//
+//  Created by Johan Thorell on 2013-09-09.
+//  Copyright (c) 2013 Team leet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SchemeApp/SchemeApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SchemeApp
+//
+//  Created by Johan Thorell on 2013-09-09.
+//  Copyright (c) 2013 Team leet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
