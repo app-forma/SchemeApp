388a3c8f4f57452189860b1868d905e1edefeadc
RikardK <rikard88karlsson@gmail.com>
2013-09-11 00:38:54 +0200

    Added LoginViewController and UserMainViewController

---
+++ SchemeApp/SchemeApp/UserMainViewController.m
@@ -0,0 +1,25 @@
+//
+//  UserMainViewController.m
+//  SchemeApp
+//
+//  Created by Rikard Karlsson on 9/11/13.
+//  Copyright (c) 2013 Team leet. All rights reserved.
+//
+
+#import "UserMainViewController.h"
+
+@interface UserMainViewController ()
+
+@end
+
+@implementation UserMainViewController
+
+
+// Ska innehålla ett sätt att välja tidsintervall på och en table view med meddelanden från lärare
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
