7c469b7ff4890e29d16209040d4114f0845963c9
Marcus Norling <marcus.norling@gmail.com>
2013-09-11 08:35:59 +0200

    Removed ensure auth & admin on CRUD routes. VG routes in comments

--- backend/app/controllers/routes.js
+++ backend/app/controllers/routes.js
@@ -5,27 +5,45 @@ module.exports = function(app) {
   app.get('/users', users.index);
   app.get('/users/:id', users.byId);
   app.get('/users-raw/:id', users.byIdRaw);
+  app.post('/users', users.create);
+  app.put('/users/:id', users.update);
+  app.del('/users/:id', users.destroy);
+
+  /* VG 
   app.post('/users', passport.ensureAuthenticated, passport.ensureAdmin(), users.create);
   app.put('/users/:id', passport.ensureAuthenticated, passport.ensureAdmin(), users.update);
   app.del('/users/:id', passport.ensureAuthenticated, passport.ensureAdmin(), users.destroy);
+  */
 
   // EventWrapper routes
   var eventWrappers = require('./eventWrappers.js');
   app.get('/eventWrappers', eventWrappers.index);
   app.get('/eventWrappers/:id', eventWrappers.byId);
   app.get('/eventWrappers-raw/:id', eventWrappers.byIdRaw);
+  app.post('/eventWrappers', eventWrappers.create);
+  app.put('/eventWrappers/:id', eventWrappers.update);
+  app.del('/eventWrappers/:id', eventWrappers.destroy);
+
+  /* VG
   app.post('/eventWrappers', passport.ensureAuthenticated, passport.ensureAdmin(), eventWrappers.create);
   app.put('/eventWrappers/:id', passport.ensureAuthenticated, passport.ensureAdmin(), eventWrappers.update);
   app.del('/eventWrappers/:id', passport.ensureAuthenticated, passport.ensureAdmin(), eventWrappers.destroy);
+  */
 
   // Event routes
   var events = require('./events.js');
   app.get('/events', events.index);
   app.get('/events/:id', events.byId);
   app.get('/events-raw/:id', events.byIdRaw);
+  app.post('/events', events.create);
+  app.put('/events/:id', events.update);
+  app.del('/events/:id', events.destroy);
+
+/* VG
   app.post('/events', passport.ensureAuthenticated, passport.ensureAdmin(), events.create);
   app.put('/events/:id', passport.ensureAuthenticated, passport.ensureAdmin(), events.update);
-  app.del('/events/:id', passport.ensureAuthenticated, passport.ensureAdmin(), events.destroy);
+  app.del('/events/:id', passport.ensureAuthenticated, passport.ensureAdmin(), events.destroy);  
+  */
 
   // Login / logout
   app.get('/logout', users.logout); 
