7185592508ce2de108127f749cae98bd6f926e50
Marcus Norling <marcus.norling@gmail.com>
2013-09-11 16:53:17 +0200

    Navcontroller hidden on loadView

---
+++ SchemeApp/SchemeApp/AdminInterfaceViewController.m
@@ -0,0 +1,38 @@
+//
+//  AdminInterfaceViewController.m
+//  SchemeApp
+//
+//  Created by Marcus Norling on 9/11/13.
+//  Copyright (c) 2013 Team leet. All rights reserved.
+//
+
+#import "AdminInterfaceViewController.h"
+
+@interface AdminInterfaceViewController ()
+
+@end
+
+@implementation AdminInterfaceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
