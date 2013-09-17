ec1b08cecb09b093e8baac60f5d87cf60170d542
Johan Thorell <jo1han@hotmail.com>
2013-09-17 10:50:02 +0200

    Users.js

--- SchemeApp/SchemeApp/LoginViewController.m
+++ SchemeApp/SchemeApp/LoginViewController.m
@@ -8,7 +8,7 @@
 
 #import "LoginViewController.h"
 #import "StudentEventMainViewController.h"
-#import "Store.h"
+
 
 @interface LoginViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *emailField;
@@ -23,6 +23,13 @@
 -(void)viewDidLoad
 {
     self.navigationController.navigationBarHidden = YES;
+    //For demo, setting a student to current user
+    Store *store = [[Store alloc]init];
+    [store setCurrentUserToUserWithEmail:@"joe@gmail.com" andPassword:nil completion:^(BOOL success) {
+        if (success) {
+            NSLog(@"%@", Store.mainStore.currentUser.docID);
+        }
+    }];
 }
 
 #pragma mark text field delegate methods:
