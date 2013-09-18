f19848c2feb403bdaab00163cb8f431d3755aafc
Henrik Holmgren <hholmgren@me.com>
2013-09-18 18:25:11 +0200

    Removed dummy code

--- SchemeApp/SchemeApp/StudentStore.m
+++ SchemeApp/SchemeApp/StudentStore.m
@@ -8,59 +8,22 @@
 
 #import "StudentStore.h"
 #import "EventWrapper.h"
-#import "Helpers.h"
-#import "User.h"
 
 
 @implementation StudentStore
-{
-    NSMutableArray *listOfEventWrappers;
-}
 
+#warning Comment
+// we should fetch all eventwrappers for a certain student? /Henrik
 - (void)eventWrappersWithStartDate:(NSDate *)startDate
                         andEndDate:(NSDate *)endDate
                         completion:(void (^)(NSArray *eventWrappers))completion
 {
     [[Store dbConnection] readByStartDate:[Helpers stringFromNSDate:startDate] toEndDate:[Helpers stringFromNSDate:endDate] callback:^(id result) {
-        
+#warning Implement
+        // Create an array with eventWrapper objects of results and send with completion
         completion(result);
         
     }];
-}
-
-#pragma mark - Extracted methods
-- (NSArray *)fillListOfEventWrappersByFilteringResult:(id)result
-                         withStartDate:(NSDate *)startDate
-                            andEndDate:(NSDate *)endDate
-{
-    [self resetListOfEventWrappers];
-    
-    for (NSDictionary *eventWrapperDictionary in result)
-    {
-        BOOL eventWrapperIsOwnedByCurrentUser = [Store.mainStore.currentUser.docID isEqualToString:[eventWrapperDictionary objectForKey:@"owner"]];
-        
-        BOOL startDateIsEqual = [[Helpers dateFromString:[eventWrapperDictionary objectForKey:@"startDate"]] isEqualToDate:startDate];
-        BOOL endDateIsEqual = [[Helpers dateFromString:[eventWrapperDictionary objectForKey:@"endDate"]] isEqualToDate:endDate];
-        BOOL dateIsEqual = startDateIsEqual && endDateIsEqual;
-        
-        if (eventWrapperIsOwnedByCurrentUser && dateIsEqual)
-        {
-            [listOfEventWrappers addObject:[[EventWrapper alloc] initWithEventWrapperDictionary:eventWrapperDictionary]];
-        }
-    }
-    
-    return listOfEventWrappers;
-}
-- (void)resetListOfEventWrappers
-{
-    if (listOfEventWrappers == nil)
-    {
-        listOfEventWrappers = [[NSMutableArray alloc] init];
-    }
-    else
-    {
-        [listOfEventWrappers removeAllObjects];
-    }
 }
 
 @end
