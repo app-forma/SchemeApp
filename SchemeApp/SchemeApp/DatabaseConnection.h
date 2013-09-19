4cb9c90130413fe917666c1958e7c4796cc0238e
Henrik Holmgren <hholmgren@me.com>
2013-09-19 10:19:15 +0200

    Added new class that will replace AFNetworking

---
+++ SchemeApp/SchemeApp/DatabaseConnection.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseConnection.h
+//  SchemeApp
+//
+//  Created by Henrik on 2013-09-19.
+//  Copyright (c) 2013 Team leet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DatabaseConnection : NSObject
+
+@end
