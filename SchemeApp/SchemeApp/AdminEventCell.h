f83908b57cac1804acbf4952434c81ffe1b145e1
Marcus Norling <marcus.norling@gmail.com>
2013-09-19 17:37:45 +0200

    Aded AdminEventCell

---
+++ SchemeApp/SchemeApp/AdminEventCell.h
@@ -0,0 +1,17 @@
+//
+//  AdminEventCell.h
+//  SchemeApp
+//
+//  Created by Marcus Norling on 9/19/13.
+//  Copyright (c) 2013 Team leet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdminEventCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *courseName;
+@property (weak, nonatomic) IBOutlet UILabel *info;
+@property (weak, nonatomic) IBOutlet UILabel *startDate;
+@property (weak, nonatomic) IBOutlet UILabel *endDate;
+
+@end
