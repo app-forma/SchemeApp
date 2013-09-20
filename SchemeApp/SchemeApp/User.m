63e5ccba1b63261ba3b1eed0bbde02f73c05149c
Henrik Holmgren <hholmgren@me.com>
2013-09-20 08:57:45 +0200

    Implemented dbSessionStore to Store and adoted User to it

--- SchemeApp/SchemeApp/User.m
+++ SchemeApp/SchemeApp/User.m
@@ -67,12 +67,37 @@
 }
 - (id)initWithUserDictionary:(NSDictionary *)userDictionary
 {
-    return  [self initWithDocID:[userDictionary objectForKey:@"_id"]
-                           Role:[User roleTypeFromString:[userDictionary objectForKey:@"role"]]
-                      firstname:[userDictionary objectForKey:@"firstname"]
-                       lastname:[userDictionary objectForKey:@"lastname"]
-                          email:[userDictionary objectForKey:@"email"]
-                       password:[userDictionary objectForKey:@"password"]];
+    self = [self initWithDocID:[userDictionary objectForKey:@"_id"]
+                          Role:[User roleTypeFromString:[userDictionary objectForKey:@"role"]]
+                     firstname:[userDictionary objectForKey:@"firstname"]
+                      lastname:[userDictionary objectForKey:@"lastname"]
+                         email:[userDictionary objectForKey:@"email"]
+                      password:[userDictionary objectForKey:@"password"]];
+#warning Testing
+    for (id messageDictionary in userDictionary[@"messages"])
+    {
+        if ([messageDictionary isKindOfClass:NSDictionary.class])
+        {
+            [self.messages addObject:[[Message alloc] initWithMsgDictionary:messageDictionary]];
+        }
+    }
+    
+    NSArray *eventWrappers = userDictionary[@"eventWrappers"];
+    NSLog(@"Test: %@", eventWrappers);
+    
+    for (id eventWrapper in userDictionary[@"eventWrappers"])
+    {
+        if ([eventWrapper isKindOfClass:NSDictionary.class])
+        {
+            [self.eventWrappers addObject:[[EventWrapper alloc] initWithEventWrapperDictionary:eventWrapper]];
+        }
+        else if ([eventWrapper isKindOfClass:NSString.class])
+        {
+            [self.eventWrappers addObject:eventWrapper];
+        }
+    }
+    
+    return self;
 }
 
 - (NSDictionary *)asDictionary
