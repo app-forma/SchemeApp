f4511c02dede1034c1f42b6df758acba42420700
Henrik Holmgren <hholmgren@me.com>
2013-09-25 20:24:41 +0200

    Added logic for adding current location by tapping MapView

--- SchemeApp/SchemeAppAdmin/SchemeAppAdmin/SchoolInfoViewController.m
+++ SchemeApp/SchemeAppAdmin/SchemeAppAdmin/SchoolInfoViewController.m
@@ -22,7 +22,7 @@
 
 @implementation SchoolInfoViewController
 {
-    MKPointAnnotation *location;
+    MKPointAnnotation *currentLocationAnnotation;
 }
 
 - (void)viewDidLoad
@@ -31,12 +31,34 @@
     
     if (Store.mainStore.currentLocation)
     {
-        [self setLocationAnnotation];
         [self setInputsToCurrentLocation];
     }
     else
     {
         self.mapView.showsUserLocation = YES;
+    }
+}
+
+- (IBAction)addCurrentLocationAnnotation:(UITapGestureRecognizer*)recognizer
+{
+    if (!self.mapHasAnnotation)
+    {
+        CLLocationCoordinate2D coordinate;
+        if (recognizer)
+        {
+            CGPoint tappedPoint = [recognizer locationInView:self.mapView];
+            coordinate = [self.mapView convertPoint:tappedPoint toCoordinateFromView:self.mapView];
+        }
+        else
+        {
+            coordinate = CLLocationCoordinate2DMake(Store.mainStore.currentLocation.latitude.doubleValue,
+                                                    Store.mainStore.currentLocation.longitude.doubleValue);
+        }
+        
+        MKPointAnnotation *pa = [[MKPointAnnotation alloc] init];
+        pa.coordinate = coordinate;
+        
+        [self.mapView addAnnotation:pa];
     }
 }
 
@@ -50,16 +72,13 @@
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
 {
     self.mapView.showsUserLocation = NO;
-    
     [self.mapView setRegion:[self regionForCoordinate:userLocation.coordinate]
                    animated:YES];
 }
 - (void)mapViewDidFinishRenderingMap:(MKMapView *)mapView fullyRendered:(BOOL)fullyRendered
 {
-    if (location && self.mapView.annotations.count == 0)
-    {
-        [self.mapView addAnnotation:location];
-    }
+#warning Testing
+//    [self addCurrentLocationAnnotation:nil];
 }
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
 {
@@ -75,27 +94,34 @@
     return pav;
 }
 
-#pragma mark - Extracted methods
-- (void)setLocationAnnotation
+#pragma mark - Queries
+- (BOOL)mapHasAnnotation
 {
-    if (!location)
-    {
-        location = [[MKPointAnnotation alloc] init];
-    }
-    
-    location.coordinate = CLLocationCoordinate2DMake(Store.mainStore.currentLocation.latitude.doubleValue,
-                                                     Store.mainStore.currentLocation.longitude.doubleValue);
+    return self.mapView.annotations.count > 0;
 }
+- (id <MKAnnotation>)mapAnnotation
+{
+    if (self.mapHasAnnotation)
+    {
+        return self.mapView.annotations[0];
+    }
+    else
+    {
+        return nil;
+    }
+}
+
+#pragma mark - Extracted methods
 - (MKCoordinateRegion)regionForCoordinate:(CLLocationCoordinate2D)coordinate
 {
     return MKCoordinateRegionMakeWithDistance(coordinate, 1000, 1000);
 }
 - (void)setInputsToCurrentLocation
 {
-    [self.mapView setRegion:[self regionForCoordinate:location.coordinate]
-                   animated:YES];
-    
     self.nameTextField.text = Store.mainStore.currentLocation.name;
+    [self.mapView setRegion:[self regionForCoordinate:CLLocationCoordinate2DMake(Store.mainStore.currentLocation.latitude.doubleValue,
+                                                                                 Store.mainStore.currentLocation.longitude.doubleValue)]
+                   animated:YES];
 }
 
 @end
