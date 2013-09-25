a31cbf1f9e74c660bca62291e143b6ed7cd3b554
RikardK <rikard88karlsson@gmail.com>
2013-09-25 22:05:47 +0200

    Popovers for adding and editing implemented. Functionality such as POSTing and PUTing is yet to be implemented

---
+++ SchemeApp/SchemeAppAdmin/SchemeAppAdmin/PopoverEventWrapperViewController.h
@@ -0,0 +1,13 @@
+//
+//  PopoverEventWrapperViewController.h
+//  SchemeAppAdmin
+//
+//  Created by Rikard Karlsson on 9/25/13.
+//  Copyright (c) 2013 Marcus Norling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopoverEventWrapperViewController : UIViewController
+
+@end
---
+++ SchemeApp/SchemeAppAdmin/SchemeAppAdmin/PopoverEventWrapperViewController.m
@@ -0,0 +1,38 @@
+//
+//  PopoverEventWrapperViewController.m
+//  SchemeAppAdmin
+//
+//  Created by Rikard Karlsson on 9/25/13.
+//  Copyright (c) 2013 Marcus Norling. All rights reserved.
+//
+
+#import "PopoverEventWrapperViewController.h"
+
+@interface PopoverEventWrapperViewController ()
+
+@end
+
+@implementation PopoverEventWrapperViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
