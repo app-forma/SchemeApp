0d8b4f47c8afb00fa7e998c7dde4663c75873463
Erik Österberg <erik82osterberg@gmail.com>
2013-09-26 14:25:30 +0200

    implemented duplicate workaround in users attendance methods

--- backend/app/controllers/users.js
+++ backend/app/controllers/users.js
@@ -2,53 +2,53 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-  Helpers = require('../../helpers.js'),
-  User = mongoose.model('User'),
-  EventWrapper = mongoose.model('EventWrapper'),
-  Message = mongoose.model('Message'),
-  passport = require('passport');
+    Helpers = require('../../helpers.js'),
+    User = mongoose.model('User'),
+    EventWrapper = mongoose.model('EventWrapper'),
+    Message = mongoose.model('Message'),
+    passport = require('passport');
 
 
 /**
  * List of users
  */
 exports.index = function (req, res) {
-  User.list(function (err, users) {
-    if (err) return res.json(500, err.errors);
-    res.json(200, users);
-  });
+    User.list(function (err, users) {
+        if (err) return res.json(500, err.errors);
+        res.json(200, users);
+    });
 };
 
 /**
  * Find item by id
  */
 exports.user = function (req, res, next, id) {
-  User.load(id, function (err, user) {
-    if (err) return next(err);
-    if (!user) return next(new Error('Failed to load user ' + id));
-    req.user = user;
-    next();
-  });
+    User.load(id, function (err, user) {
+        if (err) return next(err);
+        if (!user) return next(new Error('Failed to load user ' + id));
+        req.user = user;
+        next();
+    });
 };
 
 exports.create = function (req, res) {
-  console.log(req.body);
-  var user = new User(req.body);
-  if (typeof req.body.password === 'string') {
-    user.password = Helpers.generateCryptoPassword(user.password);
-    user.saveToDisk(user, function (err, user) {
-      if (err) {
-        console.log(err);
-        res.json(500, err.errors);
-      } else {
-        console.log(err);
-        console.log(user);
-        res.json(200, user);
-      }
-    });
-  } else {
-    res.json(500);
-  }
+    console.log(req.body);
+    var user = new User(req.body);
+    if (typeof req.body.password === 'string') {
+        user.password = Helpers.generateCryptoPassword(user.password);
+        user.saveToDisk(user, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.json(500, err.errors);
+            } else {
+                console.log(err);
+                console.log(user);
+                res.json(200, user);
+            }
+        });
+    } else {
+        res.json(500);
+    }
 };
 
 
@@ -56,198 +56,204 @@ exports.create = function (req, res) {
  * Update a user
  */
 exports.update = function (req, res) {
-  // Obj can't contain _id. Will generate error.
-  delete req.body._id;
-  User.update({
-    _id: req.params.id
-  }, req.body, {
-    upsert: true
-  }, function (err, doc) {
-    if (err) {
-      res.json(500, err);
-    } else {
-      User.findOne({
+    // Obj can't contain _id. Will generate error.
+    delete req.body._id;
+    User.update({
         _id: req.params.id
-      }).populate('eventWrappers').populate('messages')
-        .exec(function (err, doc) {
-          if (err) {
-            res.json(500, err.errors);
-          } else {
-            res.json(200, doc);
-          }
-        });
-    }
-  });
+    }, req.body, {
+        upsert: true
+    }, function (err, doc) {
+        if (err) {
+            res.json(500, err);
+        } else {
+            User.findOne({
+                _id: req.params.id
+            }).populate('eventWrappers').populate('messages')
+                .exec(function (err, doc) {
+                    if (err) {
+                        res.json(500, err.errors);
+                    } else {
+                        res.json(200, doc);
+                    }
+                });
+        }
+    });
 };
 
 /**
  * Delete a user
  */
 exports.destroy = function (req, res) {
-  User.remove({
-    _id: req.params.id
-  }, function (err) {
-    if (err) {
-      res.json(500, err.errors);
-    } else {
-      res.json(200);
-    }
-  });
+    User.remove({
+        _id: req.params.id
+    }, function (err) {
+        if (err) {
+            res.json(500, err.errors);
+        } else {
+            res.json(200);
+        }
+    });
 };
 
 /**
  * User by id
  */
 exports.byId = function (req, res) {
-  User.findOne({
-    _id: req.params.id
-  }).populate('eventWrappers').populate('messages')
-    .exec(function (err, doc) {
-      if (err) {
-        res.json(500, err.errors);
-      } else {
-        res.json(200, doc);
-      }
-    });
+    User.findOne({
+        _id: req.params.id
+    }).populate('eventWrappers').populate('messages')
+        .exec(function (err, doc) {
+            if (err) {
+                res.json(500, err.errors);
+            } else {
+                res.json(200, doc);
+            }
+        });
 };
 
 exports.byIdRaw = function (req, res) {
-  User.findOne({
-    _id: req.params.id
-  }).exec(function (err, doc) {
-    if (err) {
-      res.json(500, err.errors);
-    } else {
-      res.json(200, doc);
-    }
-  });
+    User.findOne({
+        _id: req.params.id
+    }).exec(function (err, doc) {
+        if (err) {
+            res.json(500, err.errors);
+        } else {
+            res.json(200, doc);
+        }
+    });
 };
 var populateMessagesToUser = function (messages, callback) {
-  Message.find({
-    _id: {
-      $in: messages
-    }
-  }).populate('from').exec(function (err, doc) {
-    if (err) {
-      callback(null, err);
-    } else {
-      callback(doc, null);
-    }
-  });
+    Message.find({
+        _id: {
+            $in: messages
+        }
+    }).populate('from').exec(function (err, doc) {
+        if (err) {
+            callback(null, err);
+        } else {
+            callback(doc, null);
+        }
+    });
 
 };
 var populateEventWrappersToUser = function (eventWrappers, callback) {
-  EventWrapper.find({
-    _id: {
-      $in: eventWrappers
-    }
-  }).populate('owner').populate('events').exec(function (err, doc) {
-    if (err) {
-      callback(null, e);
-    } else {
-      callback(doc, null);
+    EventWrapper.find({
+        _id: {
+            $in: eventWrappers
+        }
+    }).populate('owner').populate('events').exec(function (err, doc) {
+        if (err) {
+            callback(null, e);
+        } else {
+            callback(doc, null);
 
-    }
-  });
+        }
+    });
 };
 exports.byEmail = function (req, res) {
-  User.findOne({
-    email: req.params.email
-  }).populate('messages').populate('eventWrappers')
-    .exec(function (err, doc) {
-      if (err) {
-        res.json(500, err.errors);
-      } else {
-        if (doc !== null) {
-          populateEventWrappersToUser(doc.eventWrappers, function (evntwrps, e) {
-            if (e) {
-              console.log(e);
+    User.findOne({
+        email: req.params.email
+    }).populate('messages').populate('eventWrappers')
+        .exec(function (err, doc) {
+            if (err) {
+                res.json(500, err.errors);
             } else {
-              doc.eventWrappers = evntwrps;
-            }
-            if (doc.messages.length > 0) {
-              populateMessagesToUser(doc.messages, function (msgs, err) {
-                if (err) {
-                  console.log(err);
+                if (doc !== null) {
+                    populateEventWrappersToUser(doc.eventWrappers, function (evntwrps, e) {
+                        if (e) {
+                            console.log(e);
+                        } else {
+                            doc.eventWrappers = evntwrps;
+                        }
+                        if (doc.messages.length > 0) {
+                            populateMessagesToUser(doc.messages, function (msgs, err) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    doc.messages = msgs;
+                                    res.json(200, doc);
+                                }
+                            });
+                        } else {
+                            res.json(200, doc);
+                        }
+                    });
                 } else {
-                  doc.messages = msgs;
-                  res.json(200, doc);
+                    res.send(404);
                 }
-              });
-            } else {
-              res.json(200, doc);
             }
-          });
-        } else {
-          res.send(404);
-        }
-      }
-    });
+        });
 };
 
 exports.addAttendance = function (req, res) {
-  User.findOne({
-    _id: req.params.id
-  }).exec(function (err, user) {
-    if (err) {
-      res.json(500, err.errors);
-    } else {
-      if (req.params.date) {
-        user.attendances.push(req.params.date);
-        user.save(function (err) {
-          if (err) {
+    User.findOne({
+        _id: req.params.id
+    }).exec(function (err, user) {
+        if (err) {
             res.json(500, err.errors);
-          } else {
+        } else {
+            var foundDuplicate = false;
+            for (var i = 0; i < user.attendances.length; i++) {
+                if (req.params.date === user.attendances[i]) {
+                    foundDuplicate = true;
+                    break;
+                }
+            }
+            if (!foundDuplicate) {
+                user.attendances.push(req.params.date);
+                user.save(function (err) {
+                    if (err) {
+                        res.json(500, err.errors);
+                        return;
+                    }
+                });
+            }
             res.json(200, {
-              added: true
+                added: true
             });
-          }
-        });
-      } else {
-        res.json(500, {
-          error: "invalid data"
-        });
-      }
-    }
-  });
+        }
+    });
 };
 
 exports.removeAttendance = function (req, res) {
-  User.findOne({
-    _id: req.params.id
-  }).exec(function (err, user) {
-    if (err) {
-      res.json(500, err.errors);
-    } else {
-      console.log(req.params.date);
-      if (req.params.date) {
-        var deletions = 0;
-        user.attendances.forEach(function (date, i) {
-          if (date === req.params.date) {
-            user.attendances.splice(i, 1);
-            deletions++;
-          }
-        });
-        user.save(function (err) {
-          if (err) {
+    User.findOne({
+        _id: req.params.id
+    }).exec(function (err, user) {
+        if (err) {
             res.json(500, err.errors);
-          } else if (deletions > 0) {
-            res.json(200, {
-              deletions: deletions
-            });
-          } else {
-            res.json(500, {
-              error: "no deletions"
-            });
-          }
-        });
-      } else {
-        res.json(500, {
-          error: "invalid data"
-        });
-      }
-    }
-  });
+        } else {
+            console.log(req.params.date);
+            if (req.params.date) {
+                var deletions = 0;
+                var newArray = [];
+                for (var i = 0; i < user.attendances.length; i++) {
+                    if (req.params.date !== user.attendances[i]) {
+                        newArray.push(user.attendances[i]);
+                    } else {
+                        deletions++;
+                    }
+                }
+                user.attendances = newArray;
+                user.save(function (err) {
+                    if (err) {
+                        res.json(500, err.errors);
+                    } else if (deletions > 0) {
+                        res.json(200, {
+                            deletions: deletions
+                        });
+                    } else {
+                        res.json(500, {
+                            error: "no deletions"
+                        });
+                    }
+                });
+            } else {
+                res.json(500, {
+                    error: "invalid data"
+                });
+            }
+        }
+    });
 };
 
 
@@ -255,33 +261,33 @@ exports.removeAttendance = function (req, res) {
  *   Compare test to hashed password
  */
 exports.comparePasswords = function (password, passwordHash) {
-  return Helpers.validateCryptoPassword(password, passwordHash);
+    return Helpers.validateCryptoPassword(password, passwordHash);
 };
 
 /**
  *   Authentication
  */
 exports.login = function (req, res, next) {
-  user = req.body;
-  user.username = req.body.email;
-  passport.authenticate('local', function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      req.session.messages = [info.message];
-      return res.send(401);
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.send(200);
-    });
-  })(req, res, next);
+    user = req.body;
+    user.username = req.body.email;
+    passport.authenticate('local', function (err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            req.session.messages = [info.message];
+            return res.send(401);
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                return next(err);
+            }
+            return res.send(200);
+        });
+    })(req, res, next);
 };
 
 exports.logout = function (req, res) {
-  req.logout();
-  res.send(200);
+    req.logout();
+    res.send(200);
 };
