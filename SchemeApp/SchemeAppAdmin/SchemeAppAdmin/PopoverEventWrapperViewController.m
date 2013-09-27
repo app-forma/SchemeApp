644ac467aa3ce8e8195df25eeba01fcd68b5c1fe
Johan Thorell <jo1han@hotmail.com>
2013-09-27 21:53:49 +0200

    Eventwrappers post och put kod nästan klar

--- SchemeApp/SchemeAppAdmin/SchemeAppAdmin/PopoverEventWrapperViewController.m
+++ SchemeApp/SchemeAppAdmin/SchemeAppAdmin/PopoverEventWrapperViewController.m
@@ -21,12 +21,16 @@
 @end
 
 @implementation PopoverEventWrapperViewController
+{
+    NSString *currentMethod;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+       
     }
     return self;
 }
@@ -56,21 +60,34 @@
         self.endDate.text = [Helpers stringFromNSDate:eventWrapper.endDate];
     } else if ([method isEqualToString:@"POST"]) {
         self.courseName.text = @"";
-        self.teacher.text = @"";
+        self.teacher.text = [NSString stringWithFormat:@"%@ %@", Store.mainStore.currentUser.firstname, Store.mainStore.currentUser.lastname];
         self.litterature.text = @"";
         self.startDate.text = @"";
         self.endDate.text = @"";
+
     }
 }
-
+-(EventWrapper *)returnEventWrapperFromPopover
+{
+    EventWrapper *eventWrapper = [EventWrapper new];
+    eventWrapper.name = self.courseName.text;
+    eventWrapper.user = Store.mainStore.currentUser;
+    eventWrapper.litterature = self.litterature.text;
+    eventWrapper.startDate = [Helpers dateFromString:self.startDate.text];
+    eventWrapper.endDate = [Helpers dateFromString:self.endDate.text];
+    return eventWrapper;
+}
 - (IBAction)saveEventWrapper:(id)sender
 {
+
+//    if (self.isInEditingMode) {
+//        [self.delegate saveOrUpdateEventWrapper:[self returnEventWrapperFromPopover] for:@"PUT"];
+//    } else {
+//        [self.delegate saveOrUpdateEventWrapper:[self returnEventWrapperFromPopover] for:@"POST"];
+//    }
+   
     [self.delegate dismissPopover];
-    if (self.isInEditingMode) {
-        NSLog(@"PUT");
-    } else {
-        NSLog(@"POST");
-    }
+
 }
 
 @end
