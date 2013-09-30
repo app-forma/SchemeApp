d9e72f4854d663763aed7c1d730a9e009684f28e
Marcus Norling <marcus.norling@gmail.com>
2013-09-30 18:11:22 +0200

    Updated base64 to native methods. Removed base64lib

--- SchemeApp/SchemeApp/UIImage+Base64.m
+++ SchemeApp/SchemeApp/UIImage+Base64.m
@@ -12,15 +12,14 @@
 
 + (UIImage *)imageFromBase64:(NSString *)base64str
 {
-    NSURL *url = [NSURL URLWithString:base64str];
-    NSData *imageData = [NSData dataWithContentsOfURL:url];
-    return [UIImage imageWithData:imageData];
+    NSData *imageDataFromBase64String = [[NSData alloc]initWithBase64EncodedString:base64str options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    return [UIImage imageWithData:imageDataFromBase64String];
 }
 
 + (NSString *)base64From:(UIImage *)image
 {
-    NSData *imageData = UIImageJPEGRepresentation(image, 0.9);
-    return [imageData base64EncodedString];
+    NSData *imageData = UIImageJPEGRepresentation([UIImage imageNamed:@"sara.jpg"], 1.0);
+    return [imageData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
 }
 
 @end
