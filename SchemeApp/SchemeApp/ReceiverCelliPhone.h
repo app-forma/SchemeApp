99a2d722f7dd17967ecb4553a2cb20e2a6eadfea
Erik Österberg <erik82osterberg@gmail.com>
2013-09-30 09:54:11 +0200

    Admin functionality back on iPhone

---
+++ SchemeApp/SchemeApp/ReceiverCelliPhone.h
@@ -0,0 +1,15 @@
+//
+//  ReceiverCelliPhone.h
+//  SchemeApp
+//
+//  Created by Erik Österberg on 2013-09-30.
+//  Copyright (c) 2013 Team leet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceiverCelliPhone : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *roleLabel;
+
+@end
---
+++ SchemeApp/SchemeApp/SearchCelliPhone.m
@@ -0,0 +1,29 @@
+//
+//  SearchCelliPhone.m
+//  SchemeApp
+//
+//  Created by Erik Österberg on 2013-09-30.
+//  Copyright (c) 2013 Team leet. All rights reserved.
+//
+
+#import "SearchCelliPhone.h"
+
+@implementation SearchCelliPhone
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
