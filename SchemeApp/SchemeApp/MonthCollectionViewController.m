8c36c2cc7df24d801aab88afa93b8a3385cfadfd
Henrik Holmgren <hholmgren@me.com>
2013-10-05 07:24:07 +0200

    Arranged weeks and weekdays in calendar

--- SchemeApp/SchemeApp/MonthCollectionViewController.m
+++ SchemeApp/SchemeApp/MonthCollectionViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MonthCollectionViewController.h"
+#import "MonthCollectionReusableView.h"
 #import "DayCollectionViewCell.h"
 #import "WeekCollectionViewCell.h"
 #import "NSDate+Helpers.h"
@@ -22,14 +23,26 @@
     NSDateComponents *currentDateComponents;
     UICollectionView *currentCollectionView;
     
-    NSInteger weeksAdded;
+    NSInteger weeksAdded, emptyCellsAdded;
     BOOL didNotAddWeekCellForPrevoiusIndexPath;
 }
 
 #pragma mark - UICollectionViewDataSource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.daysInCurrentMonth + self.weeksInCurrentMonth;
+    return self.emptyCellsInCurrentMonth + self.daysInCurrentMonth + self.weeksInCurrentMonth;
+}
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView
+           viewForSupplementaryElementOfKind:(NSString *)kind
+                                 atIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ViewIdentifier = @"Month";
+    MonthCollectionReusableView *view = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader
+                                                                           withReuseIdentifier:ViewIdentifier
+                                                                                  forIndexPath:indexPath];
+    view.label.text = self.monthName;
+    
+    return view;
 }
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -39,6 +52,10 @@
     if (self.shouldAddWeekCell)
     {
         return self.weekCell;
+    }
+    else if (self.shouldAddEmptyCell)
+    {
+        return self.emptyCell;
     }
     else
     {
@@ -57,6 +74,98 @@
 }
 
 #pragma mark - Queries
+- (NSCalendar *)currentCalendar
+{
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    calendar.firstWeekday = 2;
+    
+    return calendar;
+}
+- (BOOL)shouldAddWeekCell
+{
+    BOOL firstWeekday = currentDateComponents.weekday == self.currentCalendar.firstWeekday;
+    BOOL firstCell = currentIndexPath.row == 0;
+    
+    return (firstCell || firstWeekday) && didNotAddWeekCellForPrevoiusIndexPath;
+}
+- (BOOL)shouldAddEmptyCell
+{
+    if (self.emptyCellsInCurrentMonth == emptyCellsAdded)
+    {
+        return NO;
+    }
+    else
+    {
+        return YES;
+    }
+}
+- (NSString *)monthName
+{
+    switch (self.month)
+    {
+        case 1:
+            return @"January";
+        case 2:
+            return @"February";
+        case 3:
+            return @"Mars";
+        case 4:
+            return @"April";
+        case 5:
+            return @"May";
+        case 6:
+            return @"June";
+        case 7:
+            return @"July";
+        case 8:
+            return @"August";
+        case 9:
+            return @"September";
+        case 10:
+            return @"October";
+        case 11:
+            return @"November";
+        case 12:
+            return @"December";
+        default:
+            @throw [NSException exceptionWithName:@"OutOfRangeException"
+                                           reason:@"self.month should be 1 - 12"
+                                         userInfo:nil];
+            break;
+    }
+}
+#pragma mark Number of rows
+- (NSInteger)emptyCellsInCurrentMonth
+{
+#warning Refactor
+    NSString *dateString = [NSString stringWithFormat:@"%d-%@%d-01",
+                            self.year,
+                            self.month < 10 ? @"0" : @"", self.month];
+    
+    NSDate *date = [NSDate dateFromString:dateString];
+    
+    NSDateComponents *components = [self.currentCalendar components:NSWeekdayCalendarUnit
+                                                           fromDate:date];
+    switch (components.weekday)
+    {
+        case 1:
+            return 6;
+        case 2:
+            return 0;
+        case 3:
+            return 1;
+        case 4:
+            return 2;
+        case 5:
+            return 3;
+        case 6:
+            return 4;
+        case 7:
+            return 5;
+        default:
+            return 0;
+    }
+}
 - (NSInteger)daysInCurrentMonth
 {
     switch (self.month)
@@ -99,19 +208,15 @@
                                       inUnit:NSMonthCalendarUnit
                                      forDate:[NSDate dateFromString:dateString]].length;
 }
-- (NSCalendar *)currentCalendar
+#pragma mark Cells
+- (UICollectionViewCell *)emptyCell
 {
-    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-    calendar.firstWeekday = 2;
+    static NSString *CellIdentifier = @"Empty";
+    WeekCollectionViewCell *cell = [currentCollectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier
+                                                                                    forIndexPath:currentIndexPath];
+    emptyCellsAdded++;
     
-    return calendar;
-}
-- (BOOL)shouldAddWeekCell
-{
-    BOOL firstWeekday = currentDateComponents.weekday == self.currentCalendar.firstWeekday;
-    BOOL firstDayOfMonth = currentIndexPath.row == 0;
-    
-    return (firstDayOfMonth || firstWeekday) && didNotAddWeekCellForPrevoiusIndexPath;
+    return cell;
 }
 - (WeekCollectionViewCell *)weekCell
 {
@@ -135,7 +240,7 @@
     DayCollectionViewCell *cell = [currentCollectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier
                                                                                    forIndexPath:currentIndexPath];
     
-    cell.label.text = [NSString stringWithFormat:@"%ld", (long)currentIndexPath.row + 1 - weeksAdded];
+    cell.label.text = [NSString stringWithFormat:@"%ld", (long)currentIndexPath.row + 1 - weeksAdded - emptyCellsAdded];
     cell.date = currentDate;
     
     didNotAddWeekCellForPrevoiusIndexPath = YES;
@@ -158,11 +263,10 @@
 }
 - (NSDate *)dateFromIndexPath:(NSIndexPath *)indexPath
 {
-    NSString *dateString = [NSString stringWithFormat:@"%d-%@%d-%@%d",
-                            self.year,
-                            self.month < 10 ? @"0" : @"", self.month,
-                            indexPath.row + 1 - weeksAdded < 10 ? @"0" : @"", indexPath.row + 1 - weeksAdded];
-    
+    NSInteger dayNumber = indexPath.row + 1 - emptyCellsAdded - weeksAdded;
+    NSString *dateString = [NSString stringWithFormat:@"%d-%@%d-%@%d", self.year,
+                                                                       self.month < 10 ? @"0" : @"", self.month,
+                                                                       dayNumber < 10 ? @"0" : @"", dayNumber];
     return [NSDate dateFromString:dateString];
 }
 - (NSMutableArray *)eventsFilteredByDateOfSelectedCell:(DayCollectionViewCell *)selectedCell
